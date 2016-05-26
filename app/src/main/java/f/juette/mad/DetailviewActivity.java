727dffc573314118fc105b188cbca12695ba2e4b
Ei5fuch5 <Eisfuchs@gmail.com>
2016-05-26 14:41:24 +0200

    Before contacts

--- app/src/main/java/f/juette/mad/DetailviewActivity.java
+++ app/src/main/java/f/juette/mad/DetailviewActivity.java
@@ -1,13 +1,47 @@
 package f.juette.mad;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class DetailviewActivity extends AppCompatActivity {
+
+    private Button callbackButton;
+    private long calldelay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailview);
+
+        callbackButton = (Button) findViewById(R.id.callback_button);
+        callbackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callbackCaller();
+            }
+        });
+
+        // Get the data from the OverviewActivity
+        long calltime = getIntent().getLongExtra("calltime", -1);
+        Log.i("DetailviewActivity", "calltime: " + calltime);
+
+        calldelay = System.currentTimeMillis() - calltime;
+    }
+
+    private void callbackCaller() {
+        // Log.i("DetailviewActivity", "callback: " + calldelay);
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("calldelay", calldelay);
+
+        // Data back to the Overview activity
+        setResult(Activity.RESULT_OK, returnIntent);
+
+        // closing the activity
+        finish();
     }
 }
--- app/src/main/java/f/juette/mad/OverviewActivity.java
+++ app/src/main/java/f/juette/mad/OverviewActivity.java
@@ -1,7 +1,10 @@
 package f.juette.mad;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +14,7 @@ public class OverviewActivity extends AppCompatActivity {
 
     private TextView welcomeText;
     private Button callDetailviewButton;
+    private TextView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +24,46 @@ public class OverviewActivity extends AppCompatActivity {
         // Read the view elements
         welcomeText = (TextView) findViewById(R.id.welcome_text);
         callDetailviewButton = (Button) findViewById(R.id.call_detailview_button);
+        listview = (TextView) findViewById(R.id.listview);
 
         // Actions with the elements
         callDetailviewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(OverviewActivity.this, "OnClick on Call!", Toast.LENGTH_SHORT).show();
+                callDetailviewActivity();
             }
         });
 
         // Set values/content on elements
         welcomeText.setText(R.string.welcome2);
+    }
+
+    private void callDetailviewActivity() {
+        // Toast.makeText(OverviewActivity.this, "OnClick on Call!", Toast.LENGTH_SHORT).show();
+        long calltime = System.currentTimeMillis();
+
+        // Set the activity to call
+        Intent callDetailviewIntent = new Intent(this, DetailviewActivity.class);
+
+        // Set the data for the DetailviewActivity
+        callDetailviewIntent.putExtra("calltime", calltime);
+
+        // Open the DetailviewActivity
+        startActivity(callDetailviewIntent);
+        startActivityForResult(callDetailviewIntent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK) {
+            // Return value from the DetailviewActivity
+            long calldelay = data.getLongExtra("calldelay", -1);
+            Log.i("OverviewActivity", "calldelay: " + calldelay);
+
+            listview.setText(listview.getText().toString() + "\n" + calldelay);
+        }
+
+
     }
 }
