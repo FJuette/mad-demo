b25a4f54d090bbebf79f7491e8c1a78df9bbbcf9
Ei5fuch5 <Eisfuchs@gmail.com>
2016-05-26 10:47:17 +0200

    detailview activity created

---
+++ app/src/main/java/f/juette/mad/DetailviewActivity.java
@@ -0,0 +1,13 @@
+package f.juette.mad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailview);
+    }
+}
