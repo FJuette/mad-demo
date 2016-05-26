a7d4380de812b3f50a88cd81f4447954bc00ff8c
Ei5fuch5 <Eisfuchs@gmail.com>
2016-05-26 10:13:19 +0200

    Button added TextView text change from activity Toast for button click

--- app/src/main/java/f/juette/mad/OverviewActivity.java
+++ app/src/main/java/f/juette/mad/OverviewActivity.java
@@ -2,12 +2,34 @@ package f.juette.mad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class OverviewActivity extends AppCompatActivity {
+
+    private TextView welcomeText;
+    private Button callDetailviewButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+
+        // Read the view elements
+        welcomeText = (TextView) findViewById(R.id.welcome_text);
+        callDetailviewButton = (Button) findViewById(R.id.call_detailview_button);
+
+        // Actions with the elements
+        callDetailviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(OverviewActivity.this, "OnClick on Call!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Set values/content on elements
+        welcomeText.setText(R.string.welcome2);
     }
 }
