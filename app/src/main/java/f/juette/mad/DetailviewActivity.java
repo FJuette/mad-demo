771b1d960bc2aa205342a9bc48ee2b5eb2d79bbb
Ei5fuch5 <Eisfuchs@gmail.com>
2016-05-30 08:10:20 +0200

    Input validation from EditText

--- app/src/main/java/f/juette/mad/DetailviewActivity.java
+++ app/src/main/java/f/juette/mad/DetailviewActivity.java
@@ -5,20 +5,35 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class DetailviewActivity extends AppCompatActivity {
 
     private Button callbackButton;
     private long calldelay;
+    private EditText itemNameText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailview);
 
+        itemNameText = (EditText) findViewById(R.id.item_name_text);
         callbackButton = (Button) findViewById(R.id.callback_button);
+
+        itemNameText.setOnKeyListener((v, keyCode, event) -> {
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                Log.i("DetailViewActivity", "ENTER!");
+                handleItemNameInputCompleted();
+                return true;
+            }
+            callbackButton.setEnabled(false);
+            return false;
+        });
+
         callbackButton.setOnClickListener(v -> callbackCaller());
 
         // Get the data from the OverviewActivity
@@ -26,6 +41,17 @@ public class DetailviewActivity extends AppCompatActivity {
         Log.i("DetailviewActivity", "calltime: " + calltime);
 
         calldelay = System.currentTimeMillis() - calltime;
+    }
+
+    private void handleItemNameInputCompleted() {
+        String itemName = itemNameText.getText().toString();
+        if (itemName.equals("lorem")) {
+            String errormsg = getResources().getString(R.string.detailview_itemname_errormsg, itemName);
+            itemNameText.setError(errormsg);
+        }
+        else {
+            callbackButton.setEnabled(true);
+        }
     }
 
     private void callbackCaller() {
